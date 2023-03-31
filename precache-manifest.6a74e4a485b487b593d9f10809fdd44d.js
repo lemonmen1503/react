a35be57e53a2300dab371d38ba34fdac42b398ca
lemonmen1503 <127415572+lemonmen1503@users.noreply.github.com>
2023-03-31 16:28:58 +0300

    Add files via upload

---
+++ precache-manifest.6a74e4a485b487b593d9f10809fdd44d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e821d4d6302334d5c86bd1272adb76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7262b2cd557f7d2ac38e",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "f8cda03a0698feb3be5b",
+    "url": "/static/js/2.612d2c84.chunk.js"
+  },
+  {
+    "revision": "7262b2cd557f7d2ac38e",
+    "url": "/static/js/main.075af17a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
